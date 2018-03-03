2b90d3cdd325ce11cb77ad14d0bad3f726c008ac
Lynette <lyna8907@gmail.com>
2018-03-03 10:55:07 -0600

    added minimal nav bar, projects section, contact section, fade in name

--- js/make-it-work.js
+++ js/make-it-work.js
@@ -1,9 +1,18 @@
 $('.heading h1').hide();
+$('.name').hide();
 $(window).on("scroll", function(){
     // Determine if the element is in the viewport
     if($('.screen').visible(true)) {
         $('.heading h1').addClass("type").show();
     } else {
         $('.heading h1').removeClass("type").hide();
+    }
+});
+$(window).on("scroll", function(){
+    // Determine if the element is in the viewport
+    if($('.banner-inner h1').visible(true) == false) {
+        $('.name').fadeIn();
+    } else {
+        $('.name').fadeOut();
     }
 });
